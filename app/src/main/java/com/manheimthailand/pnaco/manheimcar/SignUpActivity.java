126da5c4604b453dba2adce68a89856e4b103680
pnaco1987 <p.naco@outlook.com>
2016-10-23 10:09:45 +0700

    Choose and Setup Image finish

--- app/src/main/java/com/manheimthailand/pnaco/manheimcar/SignUpActivity.java
+++ app/src/main/java/com/manheimthailand/pnaco/manheimcar/SignUpActivity.java
@@ -1,8 +1,13 @@
 package com.manheimthailand.pnaco.manheimcar;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +20,7 @@ public class SignUpActivity extends AppCompatActivity {
     private ImageView imageView;
     private Button button;
     private String nameString, userString, passwordString, imageString;
+    private Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +54,38 @@ public class SignUpActivity extends AppCompatActivity {
             }   // onClick
         });
 
+        // Image Controller
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Please select your picture."), 1);
+
+            }   // onClick
+        });
+
     }   // Main Method
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if ((requestCode == 1) && (resultCode == RESULT_OK)) {
+            Log.d("23octV1", "Result OF");
+
+            // setupImage
+            uri = data.getData();
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }   // tryCatch
+
+        }   // if
+
+    }   // onActivityResult
 }   // Main Class
