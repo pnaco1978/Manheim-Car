5657ad22da5843a914460f7a75c0c054d5a8cb89
pnaco1987 <p.naco@outlook.com>
2016-10-30 10:39:15 +0700

    Get Location (lat, lng) finish.

--- app/src/main/java/com/manheimthailand/pnaco/manheimcar/ListService.java
+++ app/src/main/java/com/manheimthailand/pnaco/manheimcar/ListService.java
@@ -1,6 +1,11 @@
 package com.manheimthailand.pnaco.manheimcar;
 
+import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,51 +13,147 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import com.google.android.gms.location.DetectedActivity;
-
 public class ListService extends AppCompatActivity {
 
-    // Explicit
+    //Explicit
     private ListView listView;
     private String[] nameStrings, latStrings, lngStrings, imageStrings;
+    private LocationManager locationManager;
+    private Criteria criteria;
+    private double latADouble, lngADouble;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_service);
 
-        // Bind Widget
+        //Bind Widget
         listView = (ListView) findViewById(R.id.livOfficer);
 
-        // Get Value form Intent
+        //Get Value From Intent
         nameStrings = getIntent().getStringArrayExtra("Name");
         latStrings = getIntent().getStringArrayExtra("Lat");
         lngStrings = getIntent().getStringArrayExtra("Lng");
         imageStrings = getIntent().getStringArrayExtra("Image");
 
-        // Check Array
-        Log.d("24octV3", "Total records ==> " + nameStrings.length);
+        //Check Array
 
-        for (int i=0; i<nameStrings.length; i++) {
-            Log.d("24octV3", "Name (" + i + ") ==> " + nameStrings[i]);
-            Log.d("24octV3", "Image (" + i + ") ==> " + imageStrings[i]);
-            Log.d("24octV3", "Lat (" + i + ") ==> " + latStrings[i]);
-            Log.d("24octV3", "Lng (" + i + ") ==> " + lngStrings[i]);
+        Log.d("24octV3", "จำนวน Record ที่อ่านได้  ==> " + nameStrings.length);
+
+        for (int i=0;i<nameStrings.length;i++) {
+
+            Log.d("24octV3", "Name(" + i + ") ==> " + nameStrings[i]);
+            Log.d("24octV3", "Image(" + i + ") ==> " + imageStrings[i]);
+            Log.d("24octV3", "Lat(" + i + ") ==> " + latStrings[i]);
+            Log.d("24octV3", "Lng(" + i + ") ==> " + lngStrings[i]);
 
         }
 
-        // Create ListView
-        OfficerAdapter officerAdapter = new OfficerAdapter(ListService.this, nameStrings, latStrings, lngStrings, imageStrings);
+
+
+
+        //Create ListView
+        OfficerAdapter officerAdapter = new OfficerAdapter(ListService.this,
+                nameStrings, latStrings, lngStrings, imageStrings);
         listView.setAdapter(officerAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
                 Intent intent = new Intent(ListService.this, detailActivity.class);
                 startActivity(intent);
 
-            }
+            }   // onItemClick
         });
 
+        //Setup For Get Location
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+
+    }   // Main Method
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationManager.removeUpdates(locationListener);
+        latADouble = 13.719380; //Manheim location
+        lngADouble = 100.703232; //Manheim location
+
+        // Find by location
+        Location networkLocation = myFindLocation(LocationManager.NETWORK_PROVIDER);
+        if (networkLocation != null) {
+            latADouble = networkLocation.getLatitude();
+            lngADouble = networkLocation.getLongitude();
+
+        }
+
+        // Find by GPS
+        Location gpsLocation = myFindLocation(LocationManager.GPS_PROVIDER);
+        if (gpsLocation != null) {
+            latADouble = gpsLocation.getLatitude();
+            lngADouble = gpsLocation.getLongitude();
+        }
+
+        Log.d("30octV1", "lat ==> " + latADouble);
+        Log.d("30octV1", "lng ==> " + lngADouble);
+
+    }   // Override Method by Alt + Insert
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        locationManager.removeUpdates(locationListener);
+
+    }   // Override Method by Alt + Insert
+
+    public Location myFindLocation(String strProvider) {
+
+        Location location = null;
+
+        if (locationManager.isProviderEnabled(strProvider)) {
+
+            locationManager.requestLocationUpdates(strProvider, 1000, 10, locationListener);
+            location = locationManager.getLastKnownLocation(strProvider);
+
+        } else {
+            Log.d("30octV1", "Error Cannot Find Location");
+        }
+
+
+        return location;
     }
-}
+
+
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+            latADouble = location.getLatitude();
+            lngADouble = location.getLongitude();
+
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+
+
+}   // Main Class
